422626c9b7278a9d5f940f9a1632f060e3348dc7
jatan-ops <jatan.shah90@gmail.com>
2021-06-10 18:51:34 +0530

    added comments

--- functions/index.js
+++ functions/index.js
@@ -11,17 +11,17 @@ const app = express();
 app.use(authMiddleware);
 
 app.get("/", async (req, res) => {
-  const snapshot = await admin.firestore().collection("users").get();
+  const snapshot = await admin.firestore().collection("users").get(); // snapshot will get 'users' collection
 
   let users = [];
   snapshot.forEach((doc) => {
     let id = doc.id;
     let data = doc.data();
 
-    users.push({ id, ...data });
+    users.push({ id, ...data }); // spread operator will push all keys in data object
   });
 
-  res.status(200).send(JSON.stringify(users));
+  res.status(200).send(JSON.stringify(users)); // set status and send users array
 });
 
 app.get("/:id", async (req, res) => {
@@ -30,7 +30,7 @@ app.get("/:id", async (req, res) => {
     const userId = snapshot.id;
     const userData = snapshot.data();
 
-    res.status(200).send(JSON.stringify({id: userId, ...userData}));
+    res.status(200).send(JSON.stringify({id: userId, ...userData})); // set status and send object of a particular user
 })
 
 app.post("/", async (req, res) => {
@@ -38,7 +38,7 @@ app.post("/", async (req, res) => {
 
   await admin.firestore().collection("users").add(user);
 
-  res.status(201).send();
+  res.status(201).send('Successfully created'); // set status and send messasge
 });
 
 app.put("/:id", async (req, res) => {
@@ -46,20 +46,17 @@ app.put("/:id", async (req, res) => {
 
     await admin.firestore().collection('users').doc(req.params.id).update(body);
 
-    res.status(200).send()
+    res.status(200).send('Successfully updated') // set status and send message
 });
 
 app.delete("/:id", async (req, res) => {
     await admin.firestore().collection("users").doc(req.params.id).delete();
 
-    res.status(200).send();
+    res.status(200).send('Successfully deleted'); // set status and send message
 })
 
 exports.user = functions.https.onRequest(app);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
